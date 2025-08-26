5475e3be003e702f806a87d8d8b92deb46df7124
fdrcbrbr <fefo.barberi@gmail.com>
2025-08-26 07:58:38 +0200

    First commit

--- components/card.tsx
+++ components/card.tsx
@@ -1,25 +1,27 @@
 import Image from "next/image";
-import { fetchPokemon } from "@/lib/data/pokemon";
+import { getPokemonById } from "@/lib/data/pokemon";
 
 
 export default async function Card(){
 
-const pokemon = await fetchPokemon(1);
+const pokemon = await getPokemonById(1);
   return (
-    <div className="p-4 border rounded-lg shadow-lg bg-white">
+    <div className="w-40 h-50 p-4 border rounded-lg shadow-lg bg-white">
+      <div className="w-25 h-25 rounded-full overflow-hidden border-2 border-gray-500">
       <Image
        src={pokemon.pic} 
        alt={pokemon.name} 
        className="mx-auto" 
-       width={150}
-       height={150}
+       width={200}
+       height={200}
        />
+       </div>
        <p><strong>ID:</strong> {pokemon.id}</p>
        <h2 className="text-2xl font-bold capitalize">{pokemon.name}</h2>
-      <p><strong></strong> {pokemon.types.join(", ")}</p>
+      <p><strong></strong> {pokemon.types.join(" ")}</p>
       <p><strong>HP:</strong> {pokemon.HP}</p>
-      <p><strong>Attacco:</strong> {pokemon.attack}</p>
-      <p><strong>Difesa:</strong> {pokemon.defense}</p>
+      <p><strong>Attack:</strong> {pokemon.attack}</p>
+      <p><strong>Defense:</strong> {pokemon.defense}</p>
     </div>
   );
 }
--- lib/data/pokemon.ts
+++ lib/data/pokemon.ts
@@ -1,8 +1,42 @@
 import { Pokemon } from "@/lib/data/interfaces/pokemon";
 import { PokemonApiResponse } from "@/lib/data/interfaces/pokemon";
 
-export async function fetchPokemon(id: number): Promise<Pokemon> {
+export async function getPokemons(): Promise<Pokemon> {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
+  const data: PokemonApiResponse = await response.json();
+
+  const pokemon: Pokemon = {
+    id: data.id,
+    name: data.name,
+    pic: data.sprites.front_default,
+    types: data.types.map((type) => type.type.name),
+    HP: data.stats.find((stat) => stat.stat.name === "hp")!.base_stat,
+    attack: data.stats.find((stat) => stat.stat.name === "attack")!.base_stat,
+    defense: data.stats.find((stat) => stat.stat.name === "defense")!.base_stat,
+  };
+
+  return pokemon;
+};
+
+export async function getPokemonById(id: number): Promise<Pokemon> {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data: PokemonApiResponse = await response.json();
+
+  const pokemon: Pokemon = {
+    id: data.id,
+    name: data.name,
+    pic: data.sprites.front_default,
+    types: data.types.map((type) => type.type.name),
+    HP: data.stats.find((stat) => stat.stat.name === "hp")!.base_stat,
+    attack: data.stats.find((stat) => stat.stat.name === "attack")!.base_stat,
+    defense: data.stats.find((stat) => stat.stat.name === "defense")!.base_stat,
+  };
+
+  return pokemon;
+};
+
+export async function getPokemonByName(name: string): Promise<Pokemon> {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const data: PokemonApiResponse = await response.json();
 
   const pokemon: Pokemon = {
